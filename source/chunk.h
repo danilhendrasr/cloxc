38adfa3bcc40db4f434bcb2debadc09734eca1d9
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2024-11-07 16:31:27 +0700

    feat: save constants to chunk

--- source/chunk.h
+++ source/chunk.h
@@ -3,7 +3,10 @@
 
 #include <stdint.h>
 
+#include "value.h"
+
 typedef enum {
+  OP_CONSTANT,
   OP_RETURN,
 } OpCode;
 
@@ -11,10 +14,13 @@ typedef struct {
   int count;
   int capacity;
   uint8_t* code;
+  int* lines;
+  ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
--- source/debug.c
+++ source/debug.c
@@ -1,6 +1,10 @@
 #include "debug.h"
 
+#include <stdint.h>
 #include <stdio.h>
+
+#include "chunk.h"
+#include "value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name)
 {
@@ -9,6 +13,15 @@ void disassembleChunk(Chunk* chunk, const char* name)
   for (int offset = 0; offset < chunk->count;) {
     offset = disassembleInstruction(chunk, offset);
   }
+}
+
+static int constantInstruction(const char* name, Chunk* chunk, int offset)
+{
+  uint8_t constant = chunk->code[offset + 1];
+  printf("%-16s %4d '", name, offset);
+  printValue(chunk->constants.values[constant]);
+  printf("\n");
+  return offset + 2;
 }
 
 static int simpleInstruction(const char* name, int offset)
@@ -20,11 +33,18 @@ static int simpleInstruction(const char* name, int offset)
 int disassembleInstruction(Chunk* chunk, int offset)
 {
   printf("%04d ", offset);
+  if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
+    printf("  | ");
+  } else {
+    printf("%4d ", chunk->lines[offset]);
+  }
 
   uint8_t instruction = chunk->code[offset];
   switch (instruction) {
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", instruction);
+    case OP_CONSTANT:
+      return constantInstruction("OP_CONSTANT", chunk, offset);
     default:
       printf("Uknown opcode %d\n", instruction);
       return offset + 1;
