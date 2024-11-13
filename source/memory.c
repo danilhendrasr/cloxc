5f048910b8192cff487c1cf742441c5960aaae71
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2024-11-13 16:59:30 +0700

    feat: string support

--- source/memory.c
+++ source/memory.c
@@ -3,6 +3,10 @@
 
 #include <stdlib.h>
 
+#include "object.h"
+#include "value.h"
+#include "vm.h"
+
 void* reallocate(void* pointer, size_t oldSize, size_t newSize)
 {
   if (newSize == 0) {
@@ -16,4 +20,26 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize)
   }
 
   return result;
+}
+
+void freeObject(Obj* object)
+{
+  switch (object->type) {
+    case OBJ_STRING: {
+      ObjString* string = (ObjString*)object;
+      FREE_ARRAY(char, string->chars, string->length + 1);
+      FREE(ObjString, object);
+      break;
+    }
+  }
+}
+
+void freeObjects(void)
+{
+  Obj* object = vm.objects;
+  while (object != NULL) {
+    Obj* next = object->next;
+    freeObject(object);
+    object = next;
+  }
 }
